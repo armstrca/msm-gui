80d4b6cb9c5203eb1b9fe38573148cc4105c25bf
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-09-28 21:45:27 +0000

    update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,6 +6,32 @@ class MoviesController < ApplicationController
     render({ :template => "movie_templates/index" })
   end
 
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
+
+  def create
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+    
+    m.save
+
+    redirect_to("/movies")
+  end
+
   def show
     the_id = params.fetch("path_id")
 
